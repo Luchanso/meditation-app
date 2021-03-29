b644b54b78e2a9362b2a0ce5f7415e8a4b017b9d
luchanso <luchansodev@gmail.com>
2021-03-29 20:57:40 +0300

    feat: add test for root

--- src/__tests__/base.test.tsx
+++ src/__tests__/base.test.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import { View } from "react-native";
-import renderer from "react-test-renderer";
+import renderer, {
+  ReactTestRenderer,
+  ReactTestRendererJSON
+} from "react-test-renderer";
+import { App } from "../App";
 
-it(`renders correctly`, () => {
+it(`should render react native component correctly`, () => {
   const tree = renderer.create(<View>Hello world</View>).root;
   expect(tree.findByType(View)).toBeTruthy();
+});
+
+it("should render root component", () => {
+  const root = renderer.create(<App />).root;
+  expect(root.children).toHaveLength(1);
 });
