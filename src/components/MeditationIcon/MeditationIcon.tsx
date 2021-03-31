82d657a44af31cf0b36503d8734fc7b221892d9b
Oleg Loutchansky <luchansodev@gmail.com>
2021-03-31 22:41:24 +0300

    fix: props

--- src/components/MeditationIcon/MeditationIcon.tsx
+++ src/components/MeditationIcon/MeditationIcon.tsx
@@ -12,9 +12,9 @@ export const MeditationIcon = memo(function MeditationIcon({ color }: Props) {
       <Path
         d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
         stroke={stroke}
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </Svg>
   );
